5b22e4e95443f57f358875542e67203abd2aae00
fcollazos-outlier <francisco.collazos@engineering.outlier.org>
2020-09-02 11:03:44 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Text from '../src/Component/Text';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Text />
+      <div className="overlay"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Text/index.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './index.css';
+
+const Text = () => (
+  <>
+    <div className="offset-top-text"></div>
+
+    <p className="normal-text">
+      This is your <span className="big-text">LIFE</span>
+    </p>
+    <p className="normal-text">Do what you love</p>
+    <p className="big-text">AND DO IT OFTEN</p>
+
+    <p className="normal-text new-paragraph">
+      {"If you don't like something,"}
+    </p>
+    <p className="normal-text">change it!</p>
+    <p className="normal-text">
+      {"If you don't like your job,"}
+      <span className="big-text">QUIT</span>
+    </p>
+    <p className="normal-text">{"If you don't have enough time,"}</p>
+    <p className="big-text">stop whatching TV</p>
+
+    <p className="big-text new-paragraph">{"if you're looking for the love"}</p>
+    <p className="big-text">{'of your life, stop;'}</p>
+    <p className="normal-text">{'They will be waiting for you'}</p>
+    <p className="normal-text">
+      when you start <span className="big-text">doing things</span>
+    </p>
+    <p className="big-text">that you love</p>
+
+    <p className="big-text new-paragraph">{'stop overanalyzing, life is'}</p>
+    <p className="big-text">{'simple.'}</p>
+    <p className="normal-text">{'All emotions are beautiful.'}</p>
+    <p className="normal-text">{'When you eat, appreciate every'}</p>
+    <p className="normal-text">{'last bite.'}</p>
+
+    <p className="big-text new-paragraph">{'open your mind, arms and'}</p>
+    <p className="big-text">{'heart to new things and new'}</p>
+    <p className="big-text">{'people, we are united in'}</p>
+    <p className="big-text">{'our differences.'}</p>
+
+    <p className="normal-text new-paragraph">{'Ask the next person you see'}</p>
+    <p className="normal-text">
+      {'what their passion is, '}
+      <span className="big-text">{'and share'}</span>
+    </p>
+    <p className="big-text">{'your inspiring dream with them.'}</p>
+
+    <p className="normal-text new-paragraph">{'travel often;'}</p>
+    <p className="normal-text">{'Getting lost will help you'}</p>
+    <p className="normal-text">{'find yourself.'}</p>
+
+    <p className="normal-text new-paragraph">
+      {'Some opportunities come once,'}
+    </p>
+    <p className="normal-text new-paragraph">{'seize them.'}</p>
+
+    <p className="big-text new-paragraph">{'life is about the people you'}</p>
+    <p className="big-text">{'meet, and the things you'}</p>
+    <p className="big-text">{'create with them,'}</p>
+    <p className="big-text">{'so go out and start'}</p>
+    <p className="big-text">{'creating.'}</p>
+
+    <p className="big-text new-paragraph">{'life is short,'}</p>
+    <p className="big-text">{'live your dream'}</p>
+    <p className="big-text">{'and share your'}</p>
+    <p className="big-text">{'passion.'}</p>
+  </>
+);
+
+export default Text;
