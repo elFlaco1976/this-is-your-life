f4059d62e7672976450da1d51ea4907f12491332
fcollazos-outlier <francisco.collazos@engineering.outlier.org>
2020-09-02 16:56:38 -0400

    Fix responsive, small text corrections

--- src/Component/Text/index.jsx
+++ src/Component/Text/index.jsx
@@ -3,13 +3,13 @@ import './index.css';
 
 const Text = () => (
   <>
-    <div className="offset-top-text"></div>
+    <div className="offset-text"></div>
 
     <p className="normal-text">
-      This is your <span className="big-text">LIFE</span>
+      This is your <span className="big-text">life.</span>
     </p>
-    <p className="normal-text">Do what you love</p>
-    <p className="big-text">AND DO IT OFTEN</p>
+    <p className="normal-text">Do what you love,</p>
+    <p className="big-text">and do it often.</p>
 
     <p className="normal-text new-paragraph">
       {"If you don't like something,"}
@@ -17,18 +17,18 @@ const Text = () => (
     <p className="normal-text">change it!</p>
     <p className="normal-text">
       {"If you don't like your job,"}
-      <span className="big-text">QUIT</span>
+      <span className="big-text">QUIT.</span>
     </p>
     <p className="normal-text">{"If you don't have enough time,"}</p>
-    <p className="big-text">stop whatching TV</p>
+    <p className="big-text">stop whatching TV.</p>
 
-    <p className="big-text new-paragraph">{"if you're looking for the love"}</p>
-    <p className="big-text">{'of your life, stop;'}</p>
+    <p className="big-text new-paragraph">{"if you're looking for the"}</p>
+    <p className="big-text">{'love of your life, stop;'}</p>
     <p className="normal-text">{'They will be waiting for you'}</p>
     <p className="normal-text">
       when you start <span className="big-text">doing things</span>
     </p>
-    <p className="big-text">that you love</p>
+    <p className="big-text">that you love.</p>
 
     <p className="big-text new-paragraph">{'stop overanalyzing, life is'}</p>
     <p className="big-text">{'simple.'}</p>
@@ -48,14 +48,14 @@ const Text = () => (
     </p>
     <p className="big-text">{'your inspiring dream with them.'}</p>
 
-    <p className="normal-text new-paragraph">{'travel often;'}</p>
+    <p className="normal-text new-paragraph">{'Travel often;'}</p>
     <p className="normal-text">{'Getting lost will help you'}</p>
     <p className="normal-text">{'find yourself.'}</p>
 
     <p className="normal-text new-paragraph">
       {'Some opportunities come once,'}
     </p>
-    <p className="normal-text new-paragraph">{'seize them.'}</p>
+    <p className="normal-text">{'seize them.'}</p>
 
     <p className="big-text new-paragraph">{'life is about the people you'}</p>
     <p className="big-text">{'meet, and the things you'}</p>
@@ -67,6 +67,8 @@ const Text = () => (
     <p className="big-text">{'live your dream'}</p>
     <p className="big-text">{'and share your'}</p>
     <p className="big-text">{'passion.'}</p>
+
+    <div className="offset-text"></div>
   </>
 );
 
